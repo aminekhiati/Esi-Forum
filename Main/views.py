af217781a7d8795b8ea44937f76b4b0c3e9f6699
AmineKh <m.khiati@esi-sba.dz>
2019-08-07 00:52:59 +0100

    login and homePage template engine

--- Main/views.py
+++ Main/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render, redirect
-from .models import Publication
+
 from .forms import SignUpForm
 from django.http import HttpResponse
+from django.contrib.auth import login,logout,authenticate
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+
 
 
 
 def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
-        if form.is_valid(): 
+        if form.is_valid():
             utilisateur = form.save()
             utilisateur.refresh_from_db()  # load the profile instance created by the signal
             if(form.cleaned_data.get('phone_number')):
@@ -17,10 +21,37 @@ def signup(request):
                 utilisateur.profile.promotion = form.cleaned_data.get('promo')
             if(form.cleaned_data.get('bio')):
                 utilisateur.profile.bio = form.cleaned_data.get('bio')
-            utilisateur.save()            
+            utilisateur.save()
             return HttpResponse('success')
     else:
         form = SignUpForm()
     return render(request, 'Main/registration.html', {'form': form})
 
 
+def logout_request(request):
+    logout(request)
+    messages.info(request,"Logged out successfully")
+    return redirect("")
+
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, f"Logged in successfully as {username}")
+                return redirect('../home/')
+            else:
+                messages.info(request,"User dosn't exist")
+        else:
+            messages.info(request,"Invalid Syntaxe")
+    form = AuthenticationForm()
+    return render(request,"main/Home.html", {"form":form})
+
+
+
+def loggedin (request):
+    return render(request, "main/Home-Logged.html")
