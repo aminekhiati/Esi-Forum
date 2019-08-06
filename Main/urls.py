1b8e86baf8108ef49c4955c91c1925e035e7a50d
Aminekh14 <khamouda123@gmail.com>
2019-08-07 00:52:59 +0100

    login and homePage template engine

--- Main/urls.py
+++ Main/urls.py
@@ -7,5 +7,8 @@ app_name = 'Main'
 
 urlpatterns = [
     path('signup', views.signup, name='signup'),
+    path('login/', views.login_request, name='login'),
+    path('home/', views.loggedin, name='home'),
+    path('logout/', views.logout_request, name='logout')
 
 ]
